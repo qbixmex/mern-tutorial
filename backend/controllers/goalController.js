9a200c32896cc689a29ebc0d5020a69ea77e6bb6
Daniel González Briseño <daniel.gb@globant.com>
2022-02-27 16:11:50 -0600

    express-async-handler package added

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,51 +1,53 @@
+const asyncHandler = require("express-async-handler");
+
 /**
  * Get Goals
  * @route GET /app/goals
  * @access Private
  */
-const getGoals = (request, response) => {
+const getGoals = asyncHandler( async (request, response) => {
     response.status(200).json({ message: "All Goals" });
-};
+});
 
 /**
  * Get Goal
  * @route GET /app/goals/:id
  * @access Private
  */
-const getGoal = (request, response) => {
+const getGoal = asyncHandler( async (request, response) => {
     response.status(200).json({ message: `Get Goal with id: (${request.params.id})` });
-};
+});
 
 /**
  * Create Goal
  * @route POST /app/goals
  * @access Private
  */
-const createGoal = (request, response) => {
+const createGoal = asyncHandler( async (request, response) => {
     if ( !request.body.text ) {
         response.status(400);
         throw new Error("Please add a text field!");
     }
     response.status(200).json({ message: "Create a Goal" });
-};
+});
 
 /**
  * Update Goal
  * @route PATCH /app/goals/:id
  * @access Private
  */
-const updateGoal = (request, response) => {
+const updateGoal = asyncHandler( async (request, response) => {
     response.status(200).json({ message: `Update Goal with id: (${request.params.id})` });
-};
+});
 
 /**
  * Delete Goal
  * @route DELETE /app/goals/:id
  * @access Private
  */
-const deleteGoal = (request, response) => {
+const deleteGoal = asyncHandler( async (request, response) => {
     response.status(200).json({ message: `Delete Goal with id: (${request.params.id})` });
-};
+});
 
 module.exports = {
     getGoals,
